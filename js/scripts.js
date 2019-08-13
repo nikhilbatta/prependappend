9102fbebbbe54f41badba57c18dea7586730b290
nikhil batta and susan kralian <nikhil@>
2019-08-13 14:10:35 -0700

    add functionality with one alert

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,31 @@ $(document).ready(function(){
   $("#hello").click(function(){
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there</li>")
-    $("li").css('background-color','green')
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function(){
+      $(this).remove();
+    });
   });
-$("#goodbye").click(function(){
-  $("ul#user").prepend("<li>Goodbye!</li>");
-  $('ul#webpage').prepend("<li>Goodbye,dear user</li>")
-});
-$("#stop").click(function(){
-  $('ul#user').prepend("<li>Stop copying me</li>")
-  $('ul#webpage').prepend("<li>Pardon me. I meant no offense</li>")
-});
-
-
-
+  $("#goodbye").click(function(){
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $('ul#webpage').prepend("<li>Goodbye,dear user</li>")
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function(){
+      $(this).remove();
+    });
+  });
+  $("#stop").click(function(){
+    $('ul#user').prepend("<li>Stop copying me</li>")
+    $('ul#webpage').prepend("<li>Pardon me. I meant no offense</li>")
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function(){
+      $(this).remove();
+    });
+  });
 });
